2f6e3c5670999fc6e479823f9f2a07943bca13a6
songweizhi <songwz03@gmail.com>
2017-12-02 21:31:00 +1100

    v1.2

--- get_sankey_plot.R
+++ get_sankey_plot.R
@@ -1,4 +1,22 @@
 #!/usr/bin/env Rscript
+
+# Copyright (C) 2017, Weizhi Song, Torsten Thomas.
+# songwz03@gmail.com or t.thomas@unsw.edu.au
+
+# Binning_refiner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Binning_refiner is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 library(tools)
 library(optparse)
 suppressPackageStartupMessages(library(googleVis))
