036fe0b6724b4ada1cafe8c6f9183d6dff87e739
songweizhi <songwz03@gmail.com>
2019-04-28 11:20:14 +1000

    v1.4.2

--- Binning_refiner/get_sankey_plot.R
+++ Binning_refiner/get_sankey_plot.R
@@ -17,9 +17,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-suppressMessages(suppressWarnings(library(tools)))
-suppressMessages(suppressWarnings(library(optparse)))
-suppressMessages(suppressWarnings(library(googleVis)))
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg))
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = 1)
+}
+
+
+# install packages if not
+packages<-c("tools", "optparse", "googleVis")
+invisible(suppressMessages(check.packages(packages)))
 
 
 option_list = list(
